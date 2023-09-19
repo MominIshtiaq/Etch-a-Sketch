b0f1473f22eb6adaa27aeccc293b6955bf9fad4a
MominIshtiaq <momin989ishtiaq@gmail.com>
2023-09-19 18:58:40 +0500

    random and reset button functionality created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let color = "black";
+let click = true;
 
 function populateBoard(size) {
   let board = document.querySelector(".board");
@@ -24,11 +25,26 @@ function changeSize(input) {
 }
 
 function colorSquare() {
-  this.style.backgroundColor = color;
+  if (click) {
+    if (color === "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 300}, 100%, 50%)`; //! copied from StackOverFlow
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
 }
 
 function changeColor(choice) {
   color = choice;
 }
 
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
 populateBoard(16);
+document.querySelector("body").addEventListener("click", () => {
+  click = !click;
+});
