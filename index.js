6eff07603b03f1e1a434d506c39e5cbdd1206b39
MominIshtiaq <momin989ishtiaq@gmail.com>
2023-09-19 18:46:36 +0500

    black, grey and eraser Button Functionality created

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
+let color = "black";
+
 function populateBoard(size) {
   let board = document.querySelector(".board");
   let squares = board.querySelectorAll("div");
   squares.forEach((div) => div.remove());
-  board.computedStyleMap.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  board.computedStyleMap.gridTemplateRows = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
-    square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "black";
-    });
-    square.style.backgroundColor = "blue";
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
     board.insertAdjacentElement("beforeend", square);
   }
 }
 
 function changeSize(input) {
-  if (input > 2 || input <= 100) {
+  if (input >= 2 && input <= 100) {
     populateBoard(input);
   } else {
     console.log("too many squares to render");
   }
+}
+
+function colorSquare() {
+  this.style.backgroundColor = color;
+}
+
+function changeColor(choice) {
+  color = choice;
 }
 
 populateBoard(16);
