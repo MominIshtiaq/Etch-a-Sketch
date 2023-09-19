56866a8b564ce425adb8d6454e05bda5871791df
MominIshtiaq <momin989ishtiaq@gmail.com>
2023-09-19 18:35:10 +0500

    Input validation created

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-let board = document.querySelector(".board");
-board.computedStyleMap.gridTemplateColumns = "repeat(16, 1fr)";
-board.computedStyleMap.gridTemplateRows = "repeat(16, 1fr)";
-
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.style.backgroundColor = "blue";
-  board.insertAdjacentElement("beforeend", square);
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.computedStyleMap.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.computedStyleMap.gridTemplateRows = `repeat(${size}, 1fr)`;
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
+
+function changeSize(input) {
+  if (input > 2 || input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("too many squares to render");
+  }
+}
+
+populateBoard(16);
